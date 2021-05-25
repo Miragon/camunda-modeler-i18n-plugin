a3da991a3e660b5140520fe803f106fe29a21a99
Dominik Horn <dominik.horn93@gmail.com>
2021-05-25 17:52:45 +0200

    docs: add copyright

--- client/config/ConfigModal.js
+++ client/config/ConfigModal.js
@@ -1,17 +1,29 @@
-/* eslint-disable no-unused-vars */
+/**
+ * Copyright 2020 FlowSquad GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'camunda-modeler-plugin-helpers/react';
 import {Modal} from 'camunda-modeler-plugin-helpers/components';
 
-
-// polyfill upcoming structural components
 const Title = Modal.Title || (({children}) => <h2>{children}</h2>);
 const Body = Modal.Body || (({children}) => <div>{children}</div>);
 const Footer = Modal.Footer || (({children}) => <div>{children}</div>);
 
-// we can even use hooks to render into the application
+
 export default function ConfigModal({onClose}) {
 
-    // we can use the built-in styles, e.g. by adding "btn btn-primary" class names
     return (<Modal onClose={onClose}>
         <Title>
             Language changed
