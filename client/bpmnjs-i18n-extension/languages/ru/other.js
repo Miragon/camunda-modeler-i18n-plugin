3f68594989a1707be63bd0017abebdd56fd900b2
jakobmertl <jakob.mertl@miragon.io>
2023-01-19 08:18:11 +0100

    fix: merged develop from flowsquad

---
+++ client/bpmnjs-i18n-extension/languages/ru/other.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2021 FlowSquad GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This file contains translations that were used in other components.
+ */
+export default {
+    'Open minimap': 'Открыть миникарту',
+    'This maps to the process definition key.': 'Это соответствует ключу процесса.',
+    'Key': 'Ключ',
+    'Intermediate Throw Event': 'Промежуточное событие отправления',
+    'End Event': 'Конечное событие',
+    'Message Start Event': 'Начально событие сообщение',
+    'Timer Start Event': 'Начальное событие таймер',
+    'Conditional Start Event': 'Начальное событие условие',
+    'Signal Start Event': 'Начальное событие сигнал',
+    'Expand (not reversible)': 'Расширение (необратимо)',
+    'ID': 'ID',
+    'Start Event': 'Начальное событие',
+    'Message End Event': 'Конечное событие сообщение',
+    'Escalation End Event': 'Конечнеое событие эскалация',
+    'Error End Event': 'Конечное событие ошибка',
+    'Compensation End Event': 'Конечное событие компенсация',
+    'Signal End Event': 'конченое событие сигнал',
+    'Terminate End Event': 'Конечное событие останов',
+    'Transaction': 'Транзакция',
+    'Event Sub Process': 'Событийный подпроцесс',
+    'Sub Process (collapsed)': 'Подпроцесс (разрушеный)',
+    'Close minimap': 'Закрыть миникарту',
+    'Message Intermediate Catch Event': 'Промежуточное событие приема сообщения',
+    'Message Intermediate Throw Event': 'Промежуточное событие отправки сообщения',
+    'Timer Intermediate Catch Event': 'Промежуточное событие таймер',
+    'Escalation Intermediate Throw Event': 'Промежуточное событие экалация',
+    'Conditional Intermediate Catch Event': 'Промежуточное событие условие',
+    'Link Intermediate Catch Event': 'Промежуточное событие приема ссылки',
+    'Link Intermediate Throw Event': 'Промежуточное событие отправки ссылки',
+    'Compensation Intermediate Throw Event': 'Промежуточное событие компенсация',
+    'Signal Intermediate Catch Event': 'Промежуточное событие приема сигнала',
+    'Signal Intermediate Throw Event': 'Промежуточное событие отправки сигнала',
+    'Parallel Gateway': 'Параллельный шлюз',
+    'Inclusive Gateway': 'Шлюз ИЛИ',
+    'Complex Gateway': 'Сложный шлюз',
+    'Event based Gateway': 'Событийный исключающий шлюз',
+    'Exclusive Gateway': 'Исключающий шлюз',
+    'Send Task': 'Задача отправки сообщения',
+    'Receive Task': 'Задача приема сообщения',
+    'User Task': 'Пользовательская задача',
+    'Manual Task': 'Ручная операция',
+    'Business Rule Task': 'Задача бизнес-правил',
+    'Service Task': 'Сервисная задача',
+    'Script Task': 'Задача-сценарий (скрипт)',
+    'Call Activity': 'Вызывающее действие',
+    'Sub Process (expanded)': 'Подпроцесс (расширенный)',
+    'Sub Process': 'Подпроцесс',
+    'Task': 'Задача',
+    'This maps to the task definition key.': 'Это соответствует ключу задачи.',
+    'Collapsed Pool': 'Разрушенный пул',
+    'Expanded Pool': 'Расширенный пул',
+    'flow elements must be children of pools/participants': 'элементы потока должны быть дочерними элементами пулов',
+    'The follow up date as an EL expression (e.g. ${someDate} or an ISO date (e.g. 2015-06-26T09:54:00)': 'Дата последующего обновления в виде выражения EL (например, ${someDate} или дата в формате ISO (например, 2015-06-26T09:54:00)',
+    'Message Boundary Event': 'Граничащее событие сообщение',
+    'Timer Boundary Event': 'Граничащее событие таймер',
+    'Escalation Boundary Event': 'Граничащее событие эскалация',
+    'Conditional Boundary Event': 'Граничащее событие условие',
+    'Error Boundary Event': 'Граничащее событие ошибка',
+    'Cancel Boundary Event': 'Граничащее событие отмена',
+    'Signal Boundary Event': 'Граничащее событие  сигнал',
+    'Compensation Boundary Event': 'Граничащее событие компенсация',
+    'Message Boundary Event (non-interrupting)': 'Граничащее событие сообщение (непрерывное)',
+    'Timer Boundary Event (non-interrupting)': 'Граничащее событие таймер (непрерывное)',
+    'Escalation Boundary Event (non-interrupting)': 'Граничащее событие эскалация (непрерывное)',
+    'Conditional Boundary Event (non-interrupting)': 'Граничащее событие компенсация (непрерывное)',
+    'Signal Boundary Event (non-interrupting)': 'Граничащее событие  сигнал (непрерывное)',
+    'Connect using Information/Knowledge/Authority Requirement or Association': 'Подключение с использованием Информации/Знаний/Авторитетных требований или Ассоциации',
+    'Empty': 'Пусто',
+    'Literal Expression': 'Буквенное выражение',
+    'This maps to the decision definition key.': 'Это соответствует ключу решение.',
+    'Decision Table': 'Таблица решений',
+    'Output': 'Выход',
+    'Annotation': 'Аннотация',
+    'Output Expression': 'Выходное выражение',
+    'Data Type': 'Тип данных',
+    'Create new BPMN Diagram (Camunda Platform)':'Создать новую BPMN диграмму (Camunda Platform)'
+}
